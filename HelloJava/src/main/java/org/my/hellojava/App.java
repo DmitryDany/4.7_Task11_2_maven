af8c4488a013615b6d9d16d0bebe05fdb1a1de74
DmitryDany <165706692+DmitryDany@users.noreply.github.com>
2024-07-11 21:28:15 +0300

    Add files via upload

---
+++ HelloJava/src/main/java/org/my/hellojava/App.java
@@ -0,0 +1,8 @@
+package org.my.hellojava;
+
+public class App {
+    public static void main(String[] args) {
+        Text text = new Text();
+        System.out.println(text.sayHello());
+    }
+}
---
+++ HelloJava/src/main/java/org/my/hellojava/Text.java
@@ -0,0 +1,7 @@
+package org.my.hellojava;
+
+public class Text {
+    public String sayHello() {
+        return "Hello,Java!";
+    }
+}
